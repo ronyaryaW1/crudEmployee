185ee70b674b7e38aa7407e0bdb40be1e6b0eaae
Rony Arya Wijaya <bangwijay11@gmail.com>
2021-05-18 23:03:03 -0700

    add function crud

--- server/App.js
+++ server/App.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('./Employee');
 
+app.use(bodyParser.json());
+
 const Employee = mongoose.model('employee');
 
 const mongoUri =
@@ -20,7 +22,62 @@ mongoose.connection.on('error', (err) => {
   console.log('error', err);
 });
 app.get('/', (req, res) => {
-  res.send('Welcon Node');
+  Employee.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/send-data', (req, res) => {
+  const employee = new Employee({
+    name: req.body.name,
+    email: req.body.email,
+    picture: req.body.picture,
+    phone: req.body.phone,
+    salary: req.body.salary,
+    position: req.body.position,
+  });
+  employee
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/delete', (req, res) => {
+  Employee.findOneAndRemove(req.body.id)
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/update', (req, res) => {
+  Employee.findByIdAndUpdate(req.body.id, {
+    name: req.body.name,
+    email: req.body.email,
+    picture: req.body.picture,
+    phone: req.body.phone,
+    salary: req.body.salary,
+    position: req.body.position,
+  })
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, () => {
